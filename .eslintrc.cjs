e2fff46df3060d9071e43622e21d8d6a19f6b67f
AbhilashMadi <madiabhilash1563@gmail.com>
2024-01-05 23:50:32 +0530

    intialized the shadcn-ui

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "@tanstack/query"],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/indent": ["error", 2],
+    "indent": "off",
+    "quotes": ["error", "double"],
+    "react-refresh/only-export-components": [
+      "warn",
+      { "allowConstantExport": true }
+    ],
+    "react/prop-types": "off",
+    // Additional rules
+    "semi": ["error", "always"],
+    "no-console": "warn",
+    // "react/jsx-uses-react": "error",
+    // "react/jsx-uses-vars": "error",
+    // "react/react-in-jsx-scope": "off"
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+
+const App: FC = () => {
+
+  return (
+    <div>App</div>
+  );
+};
+
+export default App;
